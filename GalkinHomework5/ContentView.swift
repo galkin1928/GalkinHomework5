3814060099dd3bf8095d09b40256f028b62963d9
galkin1928 <galkin1928@gmail.com>
2024-11-13 18:02:21 +0300

    Initial Commit

---
+++ GalkinHomework5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GalkinHomework5
+//
+//  Created by GALKIN Aleksandr on 13.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GalkinHomework5/GalkinHomework5App.swift
@@ -0,0 +1,17 @@
+//
+//  GalkinHomework5App.swift
+//  GalkinHomework5
+//
+//  Created by GALKIN Aleksandr on 13.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct GalkinHomework5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
